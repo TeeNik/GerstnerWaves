4f41c2ca3239f0b8a964f6e5ed0ac98b6ac2006e
TeeNik <teenik3315@gmail.com>
2022-10-19 02:14:50 +0100

    init

---
+++ Source/GerstnerWaves/GerstnerWavesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GerstnerWavesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GERSTNERWAVES_API AGerstnerWavesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
